ba113b2e143c108c733aa75e8a2b5b560b631b0f
Kurt Seifried <kurt@seifried.org>
2021-06-12 15:32:22 -0600

    suse new script

--- scripts/mirror-www.suse.com-security-cve.sh
+++ scripts/mirror-www.suse.com-security-cve.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 wget -x https://www.suse.com/security/cve/
+
+grep "^<a href=\"CVE-" www.suse.com/security/cve/index.html  | cut -d"\"" -f2 | sed 's/^/https:\/\/www.suse.com\/security\/cve\//' > www.suse.com/security/cve/urls.txt
+
+
+wget -x -i www.suse.com/security/cve/urls.txt
