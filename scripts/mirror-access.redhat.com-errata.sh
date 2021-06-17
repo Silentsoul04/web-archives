8db5cea324949f26a971b82c8567d70a232261ac
Kurt Seifried <kurt@seifried.org>
2021-06-17 10:23:36 -0600

    Make it run faster

--- scripts/mirror-access.redhat.com-errata.sh
+++ scripts/mirror-access.redhat.com-errata.sh
@@ -4,4 +4,4 @@
 
 cat www.redhat.com/security/data/metrics/*   | grep -i RHSA | sed 's/[()"<>/,. ]/\n/g' | grep ^RHSA-[0-9][0-9][0-9][0-9]:[0-9][0-9][0-9][0-9] | sort | uniq | sed 's/^/https:\/\/access.redhat.com\/errata\//' > access.redhat.com/errata/urls.txt
 
-wget -x -i access.redhat.com/errata/urls.txt
+wget -x -nc -i access.redhat.com/errata/urls.txt
