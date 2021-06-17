2926cadbb27ac699e9248b8d06a1c38324f9e708
Kurt Seifried <kurt@seifried.org>
2021-06-16 20:23:07 -0600

    more suse stuff

--- scripts/mirror-www.suse.com-security-cve.sh
+++ scripts/mirror-www.suse.com-security-cve.sh
@@ -6,6 +6,6 @@ grep "^<a href=\"CVE-" www.suse.com/security/cve/index.html  | cut -d"\"" -f2 | 
 
 ls  www.suse.com/security/cve/CVE*.html | sed 's/^/https:\/\//' > www.suse.com/security/cve/urls-got.txt
 
-cat www.suse.com/security/cve/urls-got.txt www.suse.com/security/cve/urls-listed.txt | sort | uniq -u > www.suse.com/security/cve/urls.txt
+cat www.suse.com/security/cve/urls-got.txt www.suse.com/security/cve/urls-listed.txt | sort | uniq -u | sort -rn > www.suse.com/security/cve/urls.txt
 
 wget -x -i www.suse.com/security/cve/urls.txt
