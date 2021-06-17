4bffc25ee88570f9daf60d2c28d5ebd25083a796
Kurt Seifried <kurt@seifried.org>
2021-06-16 20:02:37 -0600

    more suse content and script update

--- scripts/mirror-www.suse.com-security-cve.sh
+++ scripts/mirror-www.suse.com-security-cve.sh
@@ -2,7 +2,10 @@
 
 wget -x https://www.suse.com/security/cve/
 
-grep "^<a href=\"CVE-" www.suse.com/security/cve/index.html  | cut -d"\"" -f2 | sed 's/^/https:\/\/www.suse.com\/security\/cve\//' > www.suse.com/security/cve/urls.txt
+grep "^<a href=\"CVE-" www.suse.com/security/cve/index.html  | cut -d"\"" -f2 | sed 's/^/https:\/\/www.suse.com\/security\/cve\//' > www.suse.com/security/cve/urls-listed.txt
 
+ls  www.suse.com/security/cve/CVE*.html | sed 's/^/https:\/\//' > www.suse.com/security/cve/urls-got.txt
+
+cat www.suse.com/security/cve/urls-got.txt www.suse.com/security/cve/urls-listed.txt | sort | uniq -u > www.suse.com/security/cve/urls.txt
 
 wget -x -i www.suse.com/security/cve/urls.txt
