6a67d4c9fd0329604a8e69c0b85cb6cf9afbe20e
Kurt Seifried <kurt@seifried.org>
2021-06-18 15:52:28 -0600

    suse

--- scripts/mirror-www.suse.com-security-cve.sh
+++ scripts/mirror-www.suse.com-security-cve.sh
@@ -2,10 +2,11 @@
 
 wget --append-output=log/www.suse.com -x https://www.suse.com/security/cve/
 
-grep "^<a href=\"CVE-" www.suse.com/security/cve/index.html  | cut -d"\"" -f2 | sed 's/^/https:\/\/www.suse.com\/security\/cve\//' > www.suse.com/security/cve/urls-listed.txt
+grep "^<a href=\"CVE-" www.suse.com/security/cve/index.html  | cut -d"\"" -f2 | sed 's/^/https:\/\/www.suse.com\/security\/cve\//' | sed 's/\.html$/\//'  > www.suse.com/security/cve/urls-listed.txt
 
-ls  www.suse.com/security/cve/CVE*.html | sed 's/^/https:\/\//' > www.suse.com/security/cve/urls-got.txt
+#ls  www.suse.com/security/cve/CVE*.html | sed 's/^/https:\/\//' > www.suse.com/security/cve/urls-got.txt
 
-cat www.suse.com/security/cve/urls-got.txt www.suse.com/security/cve/urls-listed.txt | sort | uniq -u | sort -rn > www.suse.com/security/cve/urls.txt
+#cat www.suse.com/security/cve/urls-got.txt www.suse.com/security/cve/urls-listed.txt | sort | uniq -u | sort -rn > www.suse.com/security/cve/urls.txt
 
-wget -x -i www.suse.com/security/cve/urls.txt
+wget -x -i www.suse.com/security/cve/urls-listed.txt
+
