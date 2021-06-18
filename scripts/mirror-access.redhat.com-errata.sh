c81044f6948caf0bc70d83faca6cfdfff8464fc8
Kurt Seifried <kurt@seifried.org>
2021-06-17 18:06:43 -0600

    More script cleanups and data

--- scripts/mirror-access.redhat.com-errata.sh
+++ scripts/mirror-access.redhat.com-errata.sh
@@ -2,6 +2,6 @@
 
 
 
-cat www.redhat.com/security/data/metrics/*   | grep -i RHSA | sed 's/[()"<>/,. ]/\n/g' | grep ^RHSA-[0-9][0-9][0-9][0-9]:[0-9][0-9][0-9][0-9] | sort | uniq | sed 's/^/https:\/\/access.redhat.com\/errata\//' > access.redhat.com/errata/urls.txt
+cat www.redhat.com/security/data/metrics/*   | grep -i RHSA | sed 's/[()"<>/,. ]/\n/g' | sed 's/-/:/g' | sed 's/:/-/' | sed 's/:$//' | grep "^RHSA-[0-9][0-9][0-9][0-9]:[0-9]\+$" | sort | uniq | sed 's/^/https:\/\/access.redhat.com\/errata\//' > access.redhat.com/errata/urls.txt
 
 wget --append-output=log/access.redhat.com -x -nc -i access.redhat.com/errata/urls.txt
